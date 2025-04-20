a84da86b47a681c067b8fe1b321b449cc8cf45a5
Devansh98350 <devanshukumar289@gmail.com>
2025-04-20 10:25:51 +0530

    bhu

--- src/components/Engineering/IIT/Component/Bhu/bhu.js
+++ src/components/Engineering/IIT/Component/Bhu/bhu.js
@@ -14,7 +14,11 @@ const BHU = () => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <Layout title="IIT BHU - IIT Academy">
+    <Layout
+      title="IIT BHU - IIT Academy"
+      description="IIT Bhu - Admission, CutOff, Courses, Fees, Placements, Ranking, and More."
+      keywords="IIT Bhu, IIT Bhu news, IIT Bhu Techfest, IIT Bhu fest, IIT Bhu average package, IIT Bhu campus, IIT Bhu hostel, IIT Bhu internship, IIT Bhu PhD admission, IIT Bhu packages, IIT Bhu ranking in world, IIT Bhu notable alumni, IIT Bhu fees for 4 years BTech, IIT Bhu logo, IIT Bhu fee structure, IIT Bhu courses, IIT Bhu MBA fees, IIT Bhu highest package 2024, IIT Bhu jobs, IIT Bhu admission, IIT Bhu BTech fees, IIT Bhu location, IIT Bhu MTech fees, IIT Bhu online courses, IIT Bhu electrical engineering, IIT Bhu internship 2024, IIT Bhu NIRF ranking"
+    >
       <Header />
       <div style={{ marginLeft: "2.5%", marginRight: "2.5%" }}>
         <About />
